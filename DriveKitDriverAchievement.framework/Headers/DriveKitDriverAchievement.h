d363ac56fc044a3c6e59f9178a774a358f705b3e
DriveQuant CI <developer@drivequant.com>
2020-02-10 14:06:18 +0000

    Build version 1.3.0-alpha

---
+++ DriveKitDriverAchievement.framework/Headers/DriveKitDriverAchievement.h
@@ -0,0 +1,19 @@
+//
+//  DriveKitDriverAchievement.h
+//  DriveKitDriverAchievement
+//
+//  Created by Jérémy Bayle on 06/02/2020.
+//  Copyright © 2020 DriveQuant. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DriveKitDriverAchievement.
+FOUNDATION_EXPORT double DriveKitDriverAchievementVersionNumber;
+
+//! Project version string for DriveKitDriverAchievement.
+FOUNDATION_EXPORT const unsigned char DriveKitDriverAchievementVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DriveKitDriverAchievement/PublicHeader.h>
+
+
